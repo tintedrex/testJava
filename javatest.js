bcbd67f7d2373f3dbf75576854c1839dec9b3618
tintedrex <57366008+tintedrex@users.noreply.github.com>
2025-01-02 20:00:55 +0500

    idk

---
+++ javatest.js
@@ -0,0 +1,28 @@
+const array = [1, 2, 3, 4, 5];
+array.forEach((element) => {
+    console.log(element);
+});
+
+const name = [name1, name2, name3, name4, name5];
+const grade  = [A, B, C, D, E];
+const answer = Map(name, (grade) => {
+    return name+grade;
+});
+
+
+
+const array4 = [20, 16, 15, 19, 18];
+array4.filter((array4) => {
+    return array4 >= 18;
+});
+
+
+const array5 = [60, 40, 90, 20, 120, 50];
+array5.find
+
+
+
+const arrfindIndex = [200, -450, -230, 940, 230, -670, 390];
+arrfindIndex.findIndex((element) => {
+    return element === -[arrfindIndex];
+});
